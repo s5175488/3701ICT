2ca0aeb2152f62f0e2171d2a909527406c5afbf8
s5175488 <79909590+s5175488@users.noreply.github.com>
2022-04-22 18:55:45 +1000

    Add files via upload

--- DetailView.swift
+++ DetailView.swift
@@ -1,47 +1,35 @@
-//
-//  DetailView.swift
-//  s5175488_3701ICT_Assignment_1
-//
-//  Created by Saxon Dean on 31/3/22.
-//
-
 import SwiftUI
 
-struct DetailView {
-    //Create dictionary containing the checklist, including the strings and bools
-    let itemCheckDict: [String: Bool] = [
-        "Get Milk": false,
-        "Get Bread": false,
-        "Get Dog Food": true
-    ]
-    //Returns the dictionary containg the checklist to be sorted into separate lists in other functions
-    //Unknown variable declaration error
-    return itemCheckDict
+struct DetailView: View {
+    /*
+        the following code is used to display the information about the objects when they are selcted from the MasterView
+     */
+    //A property wrapper type that subscribes to an observable object and invalidates a view whenever the observable object changes.
+    //
+    @ObservedObject var model: ItemList
+    //A property wrapper type that can read and write a value managed by SwiftUI.
+    //used to set the inital state of the url string
+    @State var url: String = ""
     
-}
+    var body: some View {
+        
+        //formats all text and images in a vertical stack
+        VStack(alignment: .center) {
+            //display the name for the object on the screen
+            TextField(ViewModel.newElementTextPlaceHolder, text: $model.name)
+                .font(.title)
+                }
+                
+                VStack(alignment: .leading){
+                    //display default text "<new>"
+                    TextField(ViewModel.newElementTextPlaceHolder,text: $model.isChecked)
+                    .multilineTextAlignment(.leading)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+            } .padding(.horizontal)
+             .fixedSize(horizontal: false, vertical: true)
 
-//Separates strings from checklist
-struct listOfItems {
-    let itemList: [String] = []
-    let itemCheckDict = DetailView()
-    
-    //Unknown variable declaration error
-    for item1 in itemCheckDict {
-        itemList.append(item1[0])
+        }
+        .padding(.bottom, 150.0)
     }
-    
-    return itemList
-}
-
-//Separates booleans from checklist 
-struct listOfBools {
-    let checkList: [Bool] = []
-    let itemCheckDict = DetailView()
-    
-    //Unknown variable declaration error
-    for item2 in itemCheckDict {
-        checkList.append(item2[1])
-    }
-    
-    return checkList
 }
---
+++ Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  s5175488_3701ICT_Assignment_1 (iOS)
+//
+//  Created by Saxon Dean on 22/4/22.
+//
+
+import Foundation
+
+class Item {
+    private var isChecked: Bool = false
+    private var listTitle: String = "To Do List"
+    private var itemName: String = ""
+}
